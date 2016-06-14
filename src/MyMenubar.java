3afc9b96508701afba364da722aa89877bf67d40
Ryoto Tomioka <tommy.ryoto93@gmail.com>
2016-06-14 21:05:55 +0900

    更新

--- src/MyMenubar.java
+++ src/MyMenubar.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.io.File;
 
 /**
  * Created by Ryoto on 2016/06/14.
@@ -6,6 +7,7 @@ import javax.swing.*;
 public class MyMenuBar extends JMenuBar {
     JMenu menu_file = new JMenu("File");
     JMenu menu_help = new JMenu("Help");
+    static MainFrame mainFrame;
 
     public MyMenuBar() {
         super();
@@ -14,6 +16,36 @@ public class MyMenuBar extends JMenuBar {
         this.add(menu_help);
 
         // ファイル
+        JMenuItem item_open_ill = new JMenuItem("Load Illuminance history");
+        item_open_ill.addActionListener(e2 -> {
+            File csv = new File("data/light.csv"); // CSVデータファイル
+            JFileChooser fileChooser = new JFileChooser(csv);
+            int selected;
+            selected = fileChooser.showOpenDialog(this);
+            if(selected == JFileChooser.APPROVE_OPTION) {
+                SimulationController.setSensorHistory(fileChooser.getSelectedFile());
+                mainFrame.changeTitle(fileChooser.getSelectedFile().getName());
+                SimulationController.isIlluminanceLoaded = true;
+                SimulationController.setIlluminanceLabelVisible(true);
+                SimulationController.checkBox_showIlluminance.setEnabled(true);
+                SimulationController.checkBox_showIlluminance.setSelected(true);
+            }
+        });
+        menu_file.add(item_open_ill);
+
+        JMenuItem item_open_lum = new JMenuItem("Load Luminosity history");
+        item_open_lum.addActionListener(e1 -> {
+            File csv = new File("data/light.csv"); // CSVデータファイル
+            JFileChooser fileChooser = new JFileChooser(csv);
+            int selected;
+            selected = fileChooser.showOpenDialog(this);
+            if(selected == JFileChooser.APPROVE_OPTION) {
+                SimulationController.setLightHistory(fileChooser.getSelectedFile());
+                mainFrame.changeTitle(fileChooser.getSelectedFile().getName());
+            }
+        });
+        menu_file.add(item_open_lum);
+
         JMenuItem item_exit = new JMenuItem("Exit");
         item_exit.addActionListener(e -> {System.exit(2);});
         menu_file.add(item_exit);
